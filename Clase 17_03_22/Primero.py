35384ffde9085b625d4936966cc25a6e34fb915b
Behindit <BehinditHidden@gmail.com>
2022-03-20 10:35:44 -0300

    Clase 17-03-22

---
+++ Clase 17_03_22/Primero.py
@@ -0,0 +1,12 @@
+# Comentario primero.py
+
+def main():
+    print("Hola Mundo")
+    print("Tendremos", 16*2, "Clases presenciales en el semestre", sep=" ")
+    print("Tendremos", 16*2, "Clases presenciales en el semestre", sep="-")
+    print("Tendremos", 16*2, "Clases presenciales en el semestre", sep="")
+    print(5/3)
+    print(5//3)
+
+
+main()
