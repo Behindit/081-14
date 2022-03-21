3762cf48c5d0e03d456a54a7ca29c405ce89ab12
Behindit <BehinditHidden@gmail.com>
2022-03-21 11:29:58 -0300

    Clase_21-03-22

---
+++ Clase_21-03-22/Segundo.py
@@ -0,0 +1,61 @@
+# #identity operator
+#
+# x = 100
+# y = 100
+#
+# print("print x: ", x,
+#       " print id(x): ", id(x),
+#       " print bin(x): ", bin(x),
+#       "print hex(x): ", hex(x))
+#
+# print("x is y: ", x is y)
+#
+# #bitwise operator (Investigar)
+#
+# a=60
+# b=13
+
+def main():
+    num1 = ingresentero(input("Ingrese un numero entero: "))
+    num2 = ingresentero(input("Ingrese un numero entero: "))
+    suma = operaEnteros(num1, num2, '+')
+    multi = operaEnteros(num1, num2, '*')
+    divnormal = operaEnteros(num1, num2, '/')
+    diventero = operaEnteros(num1, num2, '//')
+    resultados(num1, num2, suma, multi, divnormal, diventero)
+
+
+def operaEnteros(Numero1, Numero2, Operador):
+    if Operador == '+':
+        Numero1 += Numero2
+    elif Operador == '*':
+        Numero1 *= Numero2
+    elif Operador == '/':
+        Numero1 /= Numero2
+    else:
+        Numero1 //= Numero2
+
+    return Numero1
+
+
+def ingresentero(Numero):
+    Numero = int(Numero)
+    while Numero <= 0:
+        if Numero == 0:
+            Numero = input("Ingreso 0, ingrese un numero entero positivo")
+        else:
+            Numero = input(
+                "Ingreso un numero negativo, ingrese un numero entero positivo")
+    return Numero
+
+
+def resultados(Numero01, Numero02, suma, multi, divnormal, diventero):
+    print("El primer numero es: ", Numero01)
+    print("El Segundo numero es: ", Numero02)
+    print("El resultado de la suma entre el primero y el segundo con el operador += es: ", suma)
+    print("El resultado de la multiplicacion entre el primero y el segundo con el operador *= es:", multi)
+    print("El resultado de la division entre el primero y el segundo con el operador /= es:", divnormal)
+    print("El resultado de la division entera entre el primero y el segundo con el operador //= es:", diventero)
+
+
+main()
